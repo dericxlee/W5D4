8872f5d664984d09ef029c392bae5c0ed8356f0c
ningxiao <dsgrichard@hotmail.com>
2023-03-02 18:03:23 -0800

    models

--- practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/app/models/shortened_url.rb
+++ practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/app/models/shortened_url.rb
@@ -1,2 +1,42 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id           :bigint           not null, primary key
+#  short_url    :string           not null
+#  long_url     :string           not null
+#  submitter_id :bigint           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class ShortenedUrl < ApplicationRecord
+    validates :short_url, presence: true, uniqueness: true
+    validates :long_url, presence: true, uniqueness: true
+    validates :submitter_id, presence: true
+
+
+
+    def self.random_code
+        short = SecureRandom::urlsafe_base64
+        short
+    end
+    
+    # def self.create(long_url, submitter_id)
+    #     short = ShortenedUrl.random_code
+    #     ShortenedUrl.create!(short_url: short, long_url: long_url, submitter_id: submitter_id)
+    # end
+
+    after_initialize do |shortenedurl|
+        self.short_url = self.generate_short_url
+    end
+
+    private
+    def generate_short_url
+        short = ShortenedUrl.random_code
+        until !ShortenedUrl.exists?(:short_url => short)
+            short = SecureRandom::urlsafe_base64
+        end
+        short
+    end
+
 end
--- practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/db/migrate/20230303003732_create_shortened_urls.rb
+++ practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/db/migrate/20230303003732_create_shortened_urls.rb
@@ -1,7 +1,12 @@
 class CreateShortenedUrls < ActiveRecord::Migration[7.0]
   def change
     create_table :shortened_urls do |t|
+      t.string :short_url, null: false
+      t.string :long_url, null: false
+      t.references :submitter, null: false, foreign_key:{to_table: :users}
 
+      t.index :short_url, unique: true
+      t.index :long_url, unique: true
       t.timestamps
     end
   end
